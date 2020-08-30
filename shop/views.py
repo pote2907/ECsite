8d380c7a67f2618b1ebfdf72416975ee0b7b82b9
Kumet <kumeta1202@gmail.com>
2020-08-30 21:53:58 +0900

    ページネーションを導入

--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
-
+from django.core.paginator import Paginator, EmptyPage, InvalidPage
 from shop.models import Product
 
 
 def all_products(request):
     # available=TrueのProductを取得
-    products = Product.valid_objects.all()
+    products_list = Product.valid_objects.all()
+
+    # 3製品ごとに分割
+    paginator = Paginator(products_list, 3)
+    try:
+        # 何ページ目なのか取得('page'が指定されなかったときは,1を取得)
+        page = int(request.GET.get('page', '1'))
+    except:
+        page = 1
+    try:
+        # ページオブジェクト作成
+        products = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        products = paginator.page(paginator.num_pages)
+
     return render(request, 'shop/product_list.html', {'products': products})
 
 
