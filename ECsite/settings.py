7b0ada6edd6040092ba58102a6830cf2a0c2af2d
Kumet <kumeta1202@gmail.com>
2020-08-30 22:15:36 +0900

    create search app

--- ECsite/settings.py
+++ ECsite/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # my apps
     'shop.apps.ShopConfig',
+    'search.apps.SearchConfig',
 ]
 
 MIDDLEWARE = [
--- shop/views.py
+++ shop/views.py
@@ -25,7 +25,7 @@ def all_products(request):
 
 def product_detail(request, product_slug):
     try:
-        prodcut = Product.objects.get(slug=product_slug)
+        product = Product.objects.get(slug=product_slug)
     except Exception as e:
         raise e
-    return render(request, 'shop/product_detail.html', {'product': prodcut})
+    return render(request, 'shop/product_detail.html', {'product': product})
