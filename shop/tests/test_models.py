0f65e2c89dbac19784089b2017b57ae9130599e1
Kumet <kumeta1202@gmail.com>
2020-08-30 19:06:37 +0900

    create Product model

--- shop/tests/test_models.py
+++ shop/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from shop.models import Category
+from shop.models import Category, Product
 
 
 class ModelTest(TestCase):
@@ -19,3 +19,22 @@ class ModelTest(TestCase):
         category = Category.objects.all()[0]
 
         assert str(category) == category.name
+
+    def test_create_product(self):
+        category = Category.objects.create(
+            name='Black Urban Cushion',
+            slug='black-urban-cushion',
+            description='description',
+        )
+        Product.objects.create(
+            name='pag dog',
+            slug='pag-dog',
+            description='cute pag',
+            category=category,
+            price=30,
+            stock=10,
+            available=True,
+        )
+        product = Product.objects.all()[0]
+
+        assert str(product) == product.name
