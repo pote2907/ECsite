2199d27def38913775bb2742869ac0ac617d53b7
Kumet <kumeta1202@gmail.com>
2020-09-07 21:42:26 +0900

    common.py 変更

--- ECsite/settings/common.py
+++ ECsite/settings/common.py
@@ -1,18 +1,12 @@
 import os
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 's5fi*_f4j@5n8-n8wsg4=cym3cg(!_21p)g1&uw+)*0w@jai#h'
-
-
-
-
 
 # Application definition
 
@@ -99,7 +93,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
