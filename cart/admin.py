6a8381d8d719e38125d2c68cf83505dc9a89e6c7
Kumet <kumeta1202@gmail.com>
2020-11-26 18:29:45 +0900

    未使用カートを削除するadminアクションを追加

--- cart/admin.py
+++ cart/admin.py
@@ -2,8 +2,20 @@ from django.contrib import admin
 
 from .models import CartItem, Cart, OrderItem, Order
 
-# Register your models here.
-admin.site.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    actions = ['delete_unused_cart', ]
+
+    def delete_unused_cart(self, request, queryset):
+        """カートアイテムが入っていないカートを削除する"""
+        for cart in queryset:
+            cart_items = CartItem.objects.filter(cart=cart)
+            if cart_items.count() == 0:
+                cart.delete()
+
+    delete_unused_cart.short_description = 'カートアイテムが入っていないカートを削除'
+
+
+admin.site.register(Cart, CartAdmin)
 admin.site.register(CartItem)
 admin.site.register(Order)
 admin.site.register(OrderItem)
